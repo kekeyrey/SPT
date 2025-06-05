7b8dd6c2fe5a5a4226914f34d7a3ec0df04eb738
kekeyrey <lalajuna6@gmail.com>
2025-06-05 07:47:42 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="english"
+  ><head
+    ><title>ABOUT-SPT - exported project</title
+    ><meta property="og:title" content="ABOUT-SPT - exported project" /><meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    /><meta charset="utf-8" /><meta
+      property="twitter:card"
+      content="summary_large_image"
+    />
+
+    <style data-tag="reset-style-sheet">
+      html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;  -webkit-font-smoothing: antialiased;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;  color: inherit;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}details {  display: block;  margin: 0;  padding: 0;}summary::-webkit-details-marker {  display: none;}[data-thq="accordion"] [data-thq="accordion-content"] {  max-height: 0;  overflow: hidden;  transition: max-height 0.3s ease-in-out;  padding: 0;}[data-thq="accordion"] details[data-thq="accordion-trigger"][open] + [data-thq="accordion-content"] {  max-height: 1000vh;}details[data-thq="accordion-trigger"][open] summary [data-thq="accordion-icon"] {  transform: rotate(180deg);}html { scroll-behavior: smooth  }
+    </style>
+    <style data-tag="default-style-sheet">
+      html {
+        font-family: Inter;
+        font-size: 16px;
+      }
+
+      body {
+        font-weight: 400;
+        font-style:normal;
+        text-decoration: none;
+        text-transform: none;
+        letter-spacing: normal;
+        line-height: 1.15;
+        color: var(--dl-color-theme-neutral-dark);
+        background: var(--dl-color-theme-neutral-light);
+
+        fill: var(--dl-color-theme-neutral-dark);
+      }
+    </style>
+   <link rel="stylesheet" href="{{ asset('css/about-spt.css') }}">
+      ><link rel="stylesheet" href="{{ asset('css/about-spt.css') }}">
+  </head>
+  <body
+    ><link rel="stylesheet" href="./style.css" /><div
+      ><link href="./about-spt.css" rel="stylesheet" /><div
+        class="aboutspt-container"
+        ><div class="aboutsptaboutspt"
+          ><div class="aboutspt-group239229"
+            ><img
+              src="../external/gudangpakan23041-ra9x-400h.png"
+              alt="gudangpakan23041"
+              class="aboutspt-gudangpakan2"
+            /><img
+              src="../external/rectangle333041-oxl.svg"
+              alt="Rectangle333041"
+              class="aboutspt-rectangle33"
+            /><span class="aboutspt-text10">ABOUT SPT</span
+            ><span class="aboutspt-text11"
+              ><span
+                >We always try to make our customer Happy. We provide all kind
+                of facilities. </span
+              ><br /><span>Your Satisfaction is our main priority</span></span
+            ><img
+              src="../external/rectangle3623041-36gq-200h.png"
+              alt="Rectangle3623041"
+              class="aboutspt-rectangle362"
+            /><span class="aboutspt-text15"
+              ><span class="aboutspt-text16">Home</span
+              ><span> / Our Product</span></span
+            ><img
+              src="../external/line143041-9wn.svg"
+              alt="Line143041"
+              class="aboutspt-line14"
+            /><span class="aboutspt-text18"
+              >Sahabat Pakan Ternak adalah usaha yang bergerak di bidang
+              penyediaan bahan baku pakan ternak, khususnya polar, biskuit, dan
+              susu, yang diformulasikan untuk memenuhi kebutuhan nutrisi ternak
+              secara optimal. Kami memahami bahwa kualitas pakan sangat
+              menentukan kesehatan, pertumbuhan, dan produktivitas ternak, oleh
+              karena itu kami menghadirkan bahan-bahan pilihan dengan standar
+              mutu tinggi dan harga yang kompetitif. Dengan layanan yang ramah
+              dan responsif, Sahabat Pakan Ternak berkomitmen menjadi mitra
+              terbaik bagi para peternak, baik skala kecil maupun besar, yang
+              ingin meningkatkan efisiensi usaha peternakan mereka. Melalui
+              produk yang aman, bernutrisi, dan mudah diakses, kami siap
+              mendukung kemajuan peternakan Indonesia secara
+              berkelanjutan.</span
+            ><span class="aboutspt-text19"
+              >Website Sahabat Pakan Ternak dikembangkan untuk memudahkan
+              peternak dalam mendapatkan informasi seputar pakan ternak secara
+              cepat dan praktis. Melalui ini, peternak bisa membeli pakan,
+              mengetahui kandungan yang terdapat dalam pakan, dan cara
+              penggunaan yang tepat. Selain itu, website ini juga menjadi sarana
+              untuk memperluas jangkauan peternak - peternak lainnya di seluruh
+              wilayah sekitar ataupun luar sekitar.</span
+            ><div class="aboutspt-group3652"
+              ><div class="aboutspt-logo1"
+                ><span class="aboutspt-text20">SPT</span></div
+              ></div
+            ><img
+              src="../external/gudangpakan53041-h52f-400h.png"
+              alt="gudangpakan53041"
+              class="aboutspt-gudangpakan5"
+            /><div class="aboutspt-frame3876"
+              ><div class="aboutspt-frame3634"
+                ><div class="aboutspt-logo2"></div></div
+              ><div class="aboutspt-frame36171"
+                ><div class="aboutspt-group36351"
+                  ><div class="aboutspthome11"
+                    ><span class="aboutspt-text21">HOME</span></div
+                  ></div
+                ><div class="aboutspt-group36361"
+                  ><div class="aboutsptaboutspark11"
+                    ><span class="aboutspt-text22">ABOUT SPT</span></div
+                  ><img
+                    src="../external/line173123-m3op.svg"
+                    alt="Line173123"
+                    class="aboutspt-line171" /></div
+                ><div class="aboutspt-group36371"
+                  ><div class="aboutsptourproduct11"
+                    ><span class="aboutspt-text23">OUR PRODUCT</span></div
+                  ></div
+                ><div class="aboutspt-group36381"
+                  ><div class="aboutsptcontactus11"
+                    ><span class="aboutspt-text24">CONTACT US</span></div
+                  ></div
+                ></div
+              ><div class="aboutspt-frame3862"
+                ><div class="aboutspt-log1"
+                  ><div class="aboutspt-frame36461"
+                    ><span class="aboutspt-text25">Login</span></div
+                  ></div
+                ><div class="aboutspt-log2"
+                  ><div class="aboutspt-frame36462"
+                    ><span class="aboutspt-text26">Register</span></div
+                  ></div
+                ></div
+              ></div
+            ><div class="aboutspt-footer1"
+              ><div class="aboutspt-logo3"
+                ><span class="aboutspt-text27">SPT</span></div
+              ><div class="aboutspt-group2392501"
+                ><span class="aboutspt-text28"
+                  ><span>Perusahaan Peternakan terintegrasi</span><br /><span
+                    >yang memberikan produk terbaik</span
+                  ><br /><span>kepada konsumen. Kami berpengalaman</span
+                  ><br /><span>di bidang peternakan dengan systemt</span
+                  ><br /><span>eknologi terbaru, serta spesialis dalam</span
+                  ><br /><span
+                    >bidang produksi hasil pertanian dan peternakan.</span
+                  ></span
+                ><span class="aboutspt-text40">Tentang SPT</span></div
+              ></div
+            ></div
+          ><div class="aboutspt-frame239263"
+            ><div class="aboutspt-frame239260"
+              ><img
+                src="../external/image573a4bc9dbf946688e6bce2aa53ac25e33041-qmcb-300h.png"
+                alt="IMAGE573a4bc9dbf946688e6bce2aa53ac25e33041"
+                class="aboutsptimage573a4bc9dbf946688e6bce2aa53ac25e3" /><img
+                src="../external/line163041-caqo.svg"
+                alt="Line163041"
+                class="aboutspt-line16" /></div
+            ><div class="aboutspt-frame239261"
+              ><img
+                src="../external/image573a4bc9dbf946688e6bce2aa53ac25e43041-dqvd-300h.png"
+                alt="IMAGE573a4bc9dbf946688e6bce2aa53ac25e43041"
+                class="aboutsptimage573a4bc9dbf946688e6bce2aa53ac25e41" /><img
+                src="../external/line173041-mqjb.svg"
+                alt="Line173041"
+                class="aboutspt-line172" /></div
+            ><div class="aboutspt-frame239262"
+              ><img
+                src="../external/image573a4bc9dbf946688e6bce2aa53ac25e44336-oumm-300h.png"
+                alt="IMAGE573a4bc9dbf946688e6bce2aa53ac25e44336"
+                class="aboutsptimage573a4bc9dbf946688e6bce2aa53ac25e42" /><img
+                src="../external/line174336-mh6b.svg"
+                alt="Line174336"
+                class="aboutspt-line173" /></div></div
+          ><div class="aboutspt-frame239276"
+            ><div class="aboutspt-frame239274"
+              ><div class="aboutspt-frame2392751"
+                ><div class="aboutspt-frame2392641"
+                  ><span class="aboutspt-text41">Visi</span></div
+                ><span class="aboutspt-text42"
+                  ><span>Beyond No. 1, </span><br /><span
+                    >Everyday with Farmsco.</span
+                  ></span
+                ></div
+              ></div
+            ><div class="aboutspt-frame239265"
+              ><div class="aboutspt-frame2392752"
+                ><div class="aboutspt-frame2392642"
+                  ><span class="aboutspt-text46">Misi</span></div
+                ><span class="aboutspt-text47"
+                  >Memimpin Industri Peternakan babi dan Berkontribusi pada
+                  Kebahagiaan Pelanggan</span
+                ></div
+              ></div
+            ></div
+          ><div class="aboutspt-footer2"
+            ><div class="aboutspt-group2392502"
+              ><span class="aboutspt-text48"
+                ><span>Perusahaan Peternakan terintegrasi</span><br /><span
+                  >yang memberikan produk terbaik</span
+                ><br /><span>kepada konsumen. Kami berpengalaman</span
+                ><br /><span>di bidang peternakan dengan systemt</span
+                ><br /><span>eknologi terbaru, serta spesialis dalam</span
+                ><br /><span
+                  >bidang produksi hasil pertanian dan peternakan.</span
+                ></span
+              ><span class="aboutspt-text60">Tentang SPT</span></div
+            ><div class="aboutspt-logo4"
+              ><span class="aboutspt-text61">SPT</span></div
+            ></div
+          ><div class="aboutspt-frame36172"
+            ><div class="aboutspt-group36352"
+              ><div class="aboutspthome12"
+                ><span class="aboutspt-text62">HOME</span></div
+              ></div
+            ><div class="aboutspt-group36362"
+              ><div class="aboutsptaboutspark12"
+                ><span class="aboutspt-text63">ABOUT SPT</span></div
+              ></div
+            ><div class="aboutspt-group36372"
+              ><div class="aboutsptourproduct12"
+                ><span class="aboutspt-text64">OUR PRODUCT</span></div
+              ></div
+            ><div class="aboutspt-group36382"
+              ><div class="aboutsptcontactus12"
+                ><span class="aboutspt-text65">CONTACT US</span></div
+              ></div
+            ></div
+          ></div
+        ></div
+      ></div
+    >
+  </body></html
+>
