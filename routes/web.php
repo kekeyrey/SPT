e78869f361fdafcc475b72ebe58805e0c895d726
nabilp269 <naputganteng@gmail.com>
2025-06-10 11:12:36 +0700

    nyar

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('homenon');});
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', function () {return view('about');});
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', function () {return view('contact');});
 
